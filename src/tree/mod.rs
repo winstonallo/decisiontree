fbc116d3688226b026de97ae5cea4061792724b4
winstonallo <arthurbiedchar@gmail.com>
2025-03-15 17:05:02 +0100

    add: Tree as Vec<Node>

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,62 +1,173 @@
-use std::collections::HashMap;
+use std::ops::Div;
 
-use node::Node;
-use ordered_float::OrderedFloat;
+use ordered_float::{FloatIsNan, OrderedFloat};
 
-mod node;
+type Float64 = OrderedFloat<f64>;
+type NodeIndex = usize;
+type FeatureIndex = usize;
+type Threshold = Float64;
 
-pub struct DecisionTree {
-    min_samples_split: usize,
-    max_depth: usize,
-    n_features: Option<usize>,
-    root: Option<Node>,
+struct Tree {
+    root: NodeIndex,
+    nodes: Vec<Node>,
+    feature_names: Vec<String>,
 }
 
-impl DecisionTree {
-    pub fn new(min_samples_split: usize, max_depth: usize, n_features: Option<usize>) -> Self {
+enum Node {
+    Leaf(Leaf),
+    Branch(Branch),
+}
+
+struct Leaf {
+    prediction: Float64,
+    samples: usize,
+}
+
+impl Leaf {
+    pub fn new(prediction: Float64, samples: usize) -> Self {
+        Self { prediction, samples }
+    }
+}
+
+struct Branch {
+    feature: FeatureIndex,
+    threshold: Float64,
+    left: NodeIndex,
+    right: NodeIndex,
+    samples: usize,
+    prediction: Float64,
+}
+
+impl Branch {
+    pub fn new(
+        feature: FeatureIndex,
+        threshold: Float64,
+        left: NodeIndex,
+        right: NodeIndex,
+        samples: usize,
+        prediction: Float64,
+    ) -> Self {
         Self {
-            min_samples_split,
-            max_depth,
-            n_features,
-            root: None,
+            feature,
+            threshold,
+            left,
+            right,
+            samples,
+            prediction,
+        }
+    }
+}
+
+struct Feature {
+    name: String,
+    features: Vec<Float64>,
+}
+
+impl Feature {
+    pub fn new(name: &str, features: Vec<Float64>) -> Self {
+        Self {
+            name: name.to_string(),
+            features,
+        }
+    }
+}
+
+fn gini_impurity(feature: &Feature, target: &Feature) -> Float64 {
+    0.into()
+}
+
+fn get_best_feature(feature: &Feature, target: &Feature) -> Threshold {
+    let mut combined = feature
+        .features
+        .clone()
+        .into_iter()
+        .zip(target.features.clone())
+        .zip((0..feature.features.len()).collect::<Vec<FeatureIndex>>())
+        .map(|((a, b), c)| (a, b, c))
+        .collect::<Vec<(Float64, Float64, FeatureIndex)>>();
+    combined.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let x: Vec<Float64> = combined.iter().map(|f| f.0).collect();
+    let y: Vec<Float64> = combined.iter().map(|f| f.1).collect();
+    let idx: Vec<FeatureIndex> = combined.iter().map(|f| f.2).collect();
+
+    let mut lowest_impurity: Float64 = 0.into();
+    for i in 0..combined.len() - 1 {
+        let threshold: Threshold = (x[i] + x[i + 1]) / 2.0;
+    }
+
+    0.into()
+}
+
+impl Tree {
+    pub fn new(feature_names: Vec<String>) -> Self {
+        Self {
+            root: 0,
+            nodes: Vec::with_capacity(feature_names.len()),
+            feature_names,
         }
     }
 
-    pub fn fit(&mut self, x: Vec<Vec<OrderedFloat<f64>>>, y: Vec<OrderedFloat<f64>>) {
-        self.n_features = if self.n_features.is_none() {
-            Some(x[0].len())
-        } else {
-            Some(x[0].len().min(self.n_features.unwrap()))
-        };
-
-        self.root = self.grow(x, y, 0);
+    fn push(&mut self, node: Node) -> NodeIndex {
+        self.nodes.push(node);
+        self.nodes.len() - 1
     }
-
-    fn grow(&mut self, x: Vec<Vec<OrderedFloat<f64>>>, y: Vec<OrderedFloat<f64>>, depth: usize) -> Option<Node> {
-        let (n_samples, n_features) = (x.len(), x[0].len());
-        let n_labels = {
-            let mut y0 = y.clone();
-            y0.sort();
-            y0.dedup();
-            y0.len()
-        };
-
-        if depth >= self.max_depth || n_labels == 1 || n_samples < self.min_samples_split {
-            return Some(Node::from_value(self.leaf_value(y)));
+    fn grow(&mut self, depth: usize) -> NodeIndex {
+        if depth > 10 {
+            let leaf = self.push(Node::Leaf(Leaf::new(1.into(), 2)));
+            return leaf;
         }
 
-        None
-    }
+        let left = self.grow(depth + 1);
+        let right = self.grow(depth + 1);
 
-    fn leaf_value(&self, y: Vec<OrderedFloat<f64>>) -> OrderedFloat<f64> {
-        y.into_iter()
-            .fold(HashMap::<OrderedFloat<f64>, usize>::new(), |mut m, x| {
-                *m.entry(x).or_default() += 1;
-                m
-            })
-            .into_iter()
-            .max_by_key(|(_, v)| *v)
-            .map(|(k, _)| k)
-            .unwrap()
+        let name = &self.feature_names[depth % 3];
+        let node = Node::Branch(Branch::new(1, 2.into(), left, right, 1, 2.into()));
+        let node_index = self.push(node);
+
+        node_index
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::sync::LazyLock;
+
+    static X: LazyLock<Vec<Feature>> = LazyLock::new(|| {
+        vec![
+            Feature::new(
+                "loves_popcorn",
+                vec![1.into(), 1.into(), 0.into(), 0.into(), 1.into(), 1.into(), 0.into()],
+            ),
+            Feature::new(
+                "loves_soda",
+                vec![1.into(), 0.into(), 1.into(), 1.into(), 1.into(), 0.into(), 0.into()],
+            ),
+            Feature::new(
+                "age",
+                vec![
+                    7.into(),
+                    12.into(),
+                    18.into(),
+                    35.into(),
+                    38.into(),
+                    50.into(),
+                    83.into(),
+                ],
+            ),
+        ]
+    });
+
+    static Y: LazyLock<Feature> = LazyLock::new(|| {
+        Feature::new(
+            "loves_cool_as_ice",
+            vec![0.into(), 0.into(), 1.into(), 1.into(), 1.into(), 0.into(), 0.into()],
+        )
+    });
+
+    #[test]
+    fn tree_new() {
+        let tree = Tree::new(X.iter().map(|f| f.name.clone()).collect());
     }
 }
